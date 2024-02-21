60f55a3ea368f124e0dfe55ae69a4c4a6cefb5f1
yudistiraardi <mail.ardix@gmail.com>
2024-02-21 23:12:49 +0700

    feat(components): implement CustomButton behaviors

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,53 @@
+<template>
+  <button
+    @click="click"
+    :class="{ fill: fill }"
+    class="custom-button fw-semibold"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    fill: {
+      type: Boolean,
+      default: false
+    },
+    thene: {
+      type: String,
+      default: 'gray'
+    }
+  },
+
+  methods: {
+    click() {
+      this.$store.dispatch('fetch')
+    }
+  }
+}
+</script>
+
+<style>
+.custom-button {
+  width: 100%;
+  padding-block: .5rem;
+  padding-inline: 1rem;
+  border: 2px solid black;
+  border-radius: 5px;
+  background-color: white;
+  color: black;
+}
+.custom-button:hover {
+  cursor: pointer;
+}
+.fill {
+  background-color: gray;
+  color: white;
+}
+</style>
